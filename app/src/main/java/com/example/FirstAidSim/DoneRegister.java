ad3e8c2088a03ab11ef276f8a589a41b862e455b
Joann <joannchan88@gmail.com>
2021-02-26 03:34:27 -0600

    Added a Done registering page

--- app/src/main/java/com/example/FirstAidSim/DoneRegister.java
+++ app/src/main/java/com/example/FirstAidSim/DoneRegister.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DoneRegister extends AppCompatActivity {
+    final static String USER_KEY = "USER";
+    User user;
+
+
     private Button log_in_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,15 @@ public class DoneRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        if(getIntent() != null && getIntent().getExtras() != null && getIntent().hasExtra(RegisterActivity.USER_KEY)){
+            user  = (User) getIntent().getSerializableExtra(RegisterActivity.USER_KEY);
+        }
 
         log_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DoneRegister.this, MainActivity.class);
+                intent.putExtra(USER_KEY, user);
                 startActivity(intent);
             }
         });
