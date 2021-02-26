edd8f5927db62d49ee8b5ee99b71db023a610bab
MitchellMisayah <mitchellmisayah@gmail.com>
2021-02-25 21:02:43 -0600

    mitchell edit 9pm on register activity

--- app/src/main/java/com/example/FirstAidSim/RegisterActivity.java
+++ app/src/main/java/com/example/FirstAidSim/RegisterActivity.java
@@ -11,12 +11,26 @@ public class RegisterActivity extends AppCompatActivity {
     Button registerButton;
     EditText userNameR,passwordR;
 
+    //User registers . Edit: Mitchell 2/25/2021, 9pm
+    private EditText register_username;
+    private EditText register_password;
+    private EditText register_first_name;
+    private EditText register_last_name;
+    private EditText register_age;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        //EditText Objects. Edit: Mitchell 2/25/2021, 9pm
+        register_username = (EditText)findViewById(R.id.editTextTextPersonName2);
+        register_password = (EditText)findViewById(R.id.pwRegister_id);
+        register_first_name = (EditText)findViewById(R.id.editTextTextPersonName4);
+        register_last_name = (EditText)findViewById(R.id.editTextTextPersonName5);
+        register_age = (EditText)findViewById(R.id.editTextNumber);
 
 
     }
