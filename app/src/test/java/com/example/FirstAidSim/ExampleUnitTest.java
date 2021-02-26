58289b792b6e2c4162126b99b44f360786d17426
mmisay3 <mmisay3@uic.edu>
2021-02-26 08:28:20 -0600

    TestCases Added

--- app/src/test/java/com/example/FirstAidSim/ExampleUnitTest.java
+++ app/src/test/java/com/example/FirstAidSim/ExampleUnitTest.java
@@ -10,8 +10,49 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void userCount_isEmpty( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        assertEquals(0, sysBase.usersStored);
+    }
+
+    @Test
+    public void userCount_notEmpty( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        sysBase.createAccount("fn","ln,", 23, "un", "123");
+        sysBase.createAccount("aa","bb,", 23, "cc", "12");
+        assertEquals(2, sysBase.usersStored);
+    }
+
+    @Test
+    public void dataBase_search_username( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        sysBase.createAccount("fn","ln,", 23, "un", "123");
+        sysBase.createAccount("aa","bb,", 23, "cc", "12");
+        assertEquals(true, sysBase.usernameIsFound("un"));
+    }
+
+    @Test
+    public void dataBase_password_isValid( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        sysBase.createAccount("fn","ln,", 23, "un", "123");
+        sysBase.createAccount("aa","bb,", 23, "cc", "12");
+        assertEquals(true, sysBase.passwordValidation("un", "123"));
+    }
+
+    @Test
+    public void dataBase_username_caseSensitivity( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        sysBase.createAccount("first","last,", 23, "AbCdE", "123");
+        assertEquals(true, sysBase.usernameIsFound("abcde"));
+    }
+
+    @Test
+    public void dataBase_password_caseSensitivity( ){
+        SystemDatabase sysBase = new SystemDatabase();
+        sysBase.createAccount("first","last,", 23, "AbCdE", "PpPpp");
+        assertEquals(false, sysBase.passwordValidation("abcde", "ppppp"));
     }
 }
