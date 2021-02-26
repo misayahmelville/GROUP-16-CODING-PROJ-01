c6a1db08f6570ea93b14a0edede119bb4020703b
mmisay3 <mmisay3@uic.edu>
2021-02-26 02:10:20 -0600

    FirstScenario

--- app/src/main/java/com/example/FirstAidSim/MainActivity.java
+++ app/src/main/java/com/example/FirstAidSim/MainActivity.java
@@ -1,17 +1,6 @@
-// To Add
-//      1.0) Go to VCS -> Git -> Add (Ctrl + Alt + A)
-
-// To Commit
-//      2.0) Go to VCS -> Commit (Ctrl + k)
-//      2.1) On the Commit Pane (found on the left edge of your screen),
-//              check all the checkboxes, type some comment, then click "Commit"
-//              A popup box will show up prompting you to review, click "Commit" again
-
-// To Push
-//      3.0) Go to VCS -> Git -> Push (Ctrl + Shift + k)
-
 package com.example.FirstAidSim;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -22,17 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.Serializable;
 
-public class MainActivity extends AppCompatActivity {
 
-    SystemDatabase SystemDataBase = new SystemDatabase();
+public class MainActivity extends AppCompatActivity{
+
+    final static Integer REQUEST_CODE_REGISTER_ACTIVITY = 1;
+
+    static SystemDatabase SystemDataBase = new SystemDatabase();
     // Stores the registering user to a vector data structure
     // User info contains the following:
-    //  1. First Name
-    //  2. Last Name
+    //  1. First Name   4. Username (email)
+    //  2. Last Name    5. Password
     //  3. Age
-    //  4. Username (email)
-    //  5. Password
 
     protected Button login_button,register_button;
 
@@ -41,37 +32,50 @@ public class MainActivity extends AppCompatActivity {
     // ID: editTextTextEmailAddress
 
     private EditText password;
-    //Desc: User enters password
+    // Desc: User enters password
     // ID: editTextTextPassword
-    //Edit made by: Mitchell on 2/25/21, 3:30PM
-
-
+    // Edit made by: Mitchell on 2/25/21, 3:30PM
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Log in button
         login_button = (Button)findViewById(R.id.signIn_id);
         login_button.setOnClickListener((login_buttonListener));
 
-
         // Register button
         register_button = (Button)findViewById(R.id.register_id);
-        register_button.setOnClickListener(register_buttonListener);
+
+        if(getIntent() != null && getIntent().getExtras() != null && getIntent().hasExtra(RegisterActivity.USER_KEY)){
+            User user = (User) getIntent().getSerializableExtra(RegisterActivity.USER_KEY);
+            SystemDataBase.addUser(user);
+        }
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // EditText Objects
-        //Edit made by: Mitchell on 2/25/21, 3:30PM
+        // Edit made by: Mitchell on 2/25/21, 3:30PM
         username = (EditText) findViewById(R.id.editTextTextEmailAddress);
         password = (EditText) findViewById(R.id.editTextTextPassword);
     }
 
-
+    public void openLogInActivity(){
+        Intent i = new Intent(MainActivity.this,LogInActivity.class);
+        startActivity(i);
+    }
     public View.OnClickListener login_buttonListener = v -> {
-        String _username = username.toString();
-        String _password = password.toString();
+
+
+        String _username = username.getText().toString();
+        String _password = password.getText().toString();
 
         if(SystemDataBase.usernameIsFound(_username).equals(false)){
             Toast.makeText(MainActivity.this, "Username is not found", Toast.LENGTH_LONG).show();
@@ -80,27 +84,8 @@ public class MainActivity extends AppCompatActivity {
             if(SystemDataBase.passwordValidation(_username, _password))
                 openLogInActivity();
             else{
-                Toast.makeText(MainActivity.this, "Invalid Username Password",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Invalid Password",Toast.LENGTH_LONG).show();
             }
         }
     };
-
-
-    // Opens MainActivity2 , this is for the register button when clicked
-    public View.OnClickListener register_buttonListener = v -> {
-        openRegisterActivity();
-//        Log.i("TAG1", "register button clicked" );
-        String name = username.getText().toString();
-        Log.i("name", name);
-    };
-
-    public void openRegisterActivity(){
-        Intent i = new Intent(MainActivity.this,RegisterActivity.class);
-        startActivity(i);
-    }
-
-    public void openLogInActivity(){
-        Intent i = new Intent(MainActivity.this,LogInActivity.class);
-        startActivity(i);
-    }
 }
